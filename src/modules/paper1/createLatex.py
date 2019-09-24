ece1bee6f33eb5e7b22250aa695fd83a26912cc4
geraldinepyh <geraldine.pang@holmusk.com>
2019-09-24 16:34:34 +0800

    completed latex library.

--- src/modules/paper1/createLatex.py
+++ src/modules/paper1/createLatex.py
@@ -1,88 +1,31 @@
-from logs import logDecorator as lD 
-import jsonref, pprint
-import os
-
-from lib.databaseIO import pgIO 
-from psycopg2 import sql
-import numpy as np
+from lib.LaTeXreport import reportWriter as rw
 import pandas as pd
-import pickle
 
-from lib.LaTeXreport import createLatex as lt
-import pylatex
-from pylatex import Document, Section, Subsection, Tabular, Tabularx, MultiColumn, MultiRow, Package, NoEscape, Command, Figure
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+df1 = df.groupby('species').mean()
+df2 = df.groupby('species').min()
+df3 = df.groupby('species').max()
 
-config = jsonref.load(open('../config/config.json'))
-logBase = config['logging']['logBase'] + '.modules.paper1.createLatex'
+rep = rw.Report('Top_Secret_Project')
+rep.title = "My Top Secret Project Report"
+rep.initialize(rep.fpath)
 
+# Add media 
+rep.saveFigure('Figure1', '/home/gerpang/LaTeXreport/results/Paper1/s0102.png', caption='This is the first figure.', option='scale=0.8', override=True)
+rep.saveFigure('Figure2', '/home/gerpang/LaTeXreport/results/Paper1/figure1.png', caption='This is the second figure.', option='scale=0.6', override=True)
+rep.saveTable('Table1', df1, caption='Table of results 1.')
+rep.saveTable('Table2', df2, caption='Table of past results.', override=False)
+rep.saveTable('Table3', df3, caption='Table of new results.', override=True)
 
-@lD.log(logBase + '.main')
-def main(logger, resultsDict):
-    '''
-    Creates a sample report in pdf format.
-    '''
-    try:
-        
-        jsonConfig = jsonref.load(open('../config/paper1/createLatex.json'))
-        tables = jsonConfig["tables"] 
-        # Creates Tables
-        for tbl in tables:
-            lt.saveTable(table = jsonConfig["tables"][tbl]["files"],  
-                fname = "../results/Paper1/{}".format(tbl))
+# Add sections
+rep.addSection('Introduction')
+rep.addSection('Methodology')
+rep.addSection('Data Collection', level=2)
+rep.addSection('Models', level=2)
+rep.addSection('Results')
+rep.addSection('Discussion')
+rep.addSection('Conclusion')
+print(rep.sections)
 
-        # Creates PDF Document
-        makeReport(exportAsPDF=True)
-
-        return
-    except Exception as e: 
-        logger.error(f'Unable to run main \n {e}')
-
-@lD.log(logBase + '.makeReport')
-def makeReport(logger, exportAsPDF = True):
-    '''
-    String pieces of latex together to form a report. 
-    Assumes Latex have already been written to the same output location.
-    '''
-    try:
-        jsonConfig = jsonref.load(open('../config/paper1/createLatex.json'))
-        fpath = jsonConfig['output']['savePath']
-
-        # # Configure Document 
-        doc = Document()
-        packages = jsonConfig["packages"]
-        for p in packages: doc.packages.append(Package(p))
-        preambles = jsonConfig["preamble"]
-        # Add Title/Authors
-        for pa in preambles:
-            doc.preamble.append(Command(pa, preambles[pa]))
-        doc.append(NoEscape(r"\maketitle"))
-        # Abstract          
-        doc.append(NoEscape(r"\begin{abstract}"))
-        doc.append(jsonConfig["abstract"])
-        doc.append(NoEscape(r"\end{abstract}"))
-
-        # # # String pieces into a report
-        lt.addSections(doc, 1)
-        lt.addTable(doc, 2, wideTable = True)
-        lt.addSections(doc, 2)     
-        lt.addTable(doc, 3)
-        lt.addSections(doc, 3)     
-        lt.addTable(doc, 4)
-
-        # # Add image
-        # lt.addFigure(doc, 1)
-        for fig in jsonConfig["figures"]:
-            with doc.create(Figure(position='h!')) as fig_:
-                fig_.add_image("Paper1/" + jsonConfig["figures"][fig]["filename"])
-                fig_.add_caption(jsonConfig["figures"][fig]["caption"])
-
-        # # Build the document
-        if exportAsPDF:
-            doc.generate_pdf(fpath+"finalReport")
-        else:
-            doc.generate_tex(fpath+"finalReport")
-
-        return doc
-    except Exception as e: 
-        logger.error(f'Unable to run makeReport \n {e}')
-
+# Generate PDF/Tex
+rep.makeReport(tex_only=False)
